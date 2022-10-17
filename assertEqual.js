dfb140c9dd573487a043379ea74be44777cf615e
Cheryet <C.heryet@outlook.com>
2022-10-17 15:59:09 -0600

    Updated Template Strings

--- assertEqual.js
+++ assertEqual.js
@@ -1,9 +1,9 @@
 let assertEqual = function(actual, expected) {
 
   if (actual === expected) {
-    console.log('✅ Assertion Passed ✅: [actual] === [expected]');
+    console.log(`✅ Assertion Passed ✅: ${actual} === ${expected}`);
   } else {
-    console.log("🚫 Assertion Failed 🚫: [actual] !== [expected]");
+    console.log(`🚫 Assertion Failed 🚫: ${actual} !== ${expected}`);
   }
   
 };
