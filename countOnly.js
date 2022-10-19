e0d6a390148fca9051e58070f5495620b91b8ea4
Cheryet <C.heryet@outlook.com>
2022-10-19 14:27:39 -0600

    Added countOnly.js

---
+++ countOnly.js
@@ -0,0 +1,46 @@
+let assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed ✅: ${actual} === ${expected}`);
+  } else {
+    console.log(`🚫 Assertion Failed 🚫: ${actual} !== ${expected}`);
+  }
+  
+};
+
+const countOnly = function(allItems, itemsToCount) {
+  const results = {}
+
+  for (let item of allItems){
+    if (itemsToCount[item]){
+      if (results[item]){
+        results[item] += 1
+      } else {
+        results[item] = 1
+      }
+    }
+  }
+  return results;
+
+}
+
+// ~~~ Tests ~~~
+
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);
